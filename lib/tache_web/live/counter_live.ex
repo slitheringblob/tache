e384de681a53752ee33dd7c262c5cd989d70b3f0
slitheringblob <jaydeepgantra11@gmail.com>
2023-01-17 20:30:11 +0530

    refactored into seperate template file for counter

--- lib/tache_web/live/counter_live.ex
+++ lib/tache_web/live/counter_live.ex
@@ -5,30 +5,6 @@ defmodule TacheWeb.CounterLive do
     {:ok, assign(socket, number: 0, diff_amount: 0)}
   end
 
-  def render(assigns) do
-    ~H"""
-      <br>
-      <span ><strong>Counter:</strong></span> <%= @number %> <br><br>
-      <strong>Singluar Increments/Decrements</strong><br>
-      <button phx-click="add">Add</button>
-      <button phx-click="subtract">Subtract</button><br><br>
-
-      <strong>Chonk Increments(Empty values default to 1)</strong>
-      <.form let={f} for={:add_form} phx-submit="add_by">
-        <%= text_input f, :diff_amount %>
-        <button>Add </button>
-      </.form>
-
-      <strong>Chonk Decrements(Empty values default to 1)</strong>
-      <.form let={f} for={:reduce_form} phx-submit="reduce_by">
-        <%= text_input f, :diff_amount %>
-        <button>Subtract </button>
-      </.form>
-      <span>(Refresh the page to reset the counter to zero!)</span>
-    """
-  end
-
-
   def handle_event("add", _params, socket) do
     {:noreply, assign(socket, number: socket.assigns.number + 1)}
   end
